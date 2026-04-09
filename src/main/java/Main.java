fb0cf65d6f788aaccd2aaa0149f05d78abf68a7f
Harsha <harshachandim92@gmail.com>
2026-04-09 14:29:10 +0530

    Set up Grizzly HTTP server with JAX-RS application, restructure project files, and adjust Jersey dependencies in pom.xml

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.net.URI;
+
+public class Main {
+
+    public static final String BASE_URI = "http://localhost:8080/api/v1/";
+
+    public static HttpServer startServer() {
+        final ResourceConfig rc = new ResourceConfig().packages("resource");
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
+    public static void main(String[] args) {
+        final HttpServer server = startServer();
+        System.out.println("Server started at " + BASE_URI);
+
+        try {
+            Thread.currentThread().join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -1,7 +1,8 @@
+package config;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/api/v1")
 public class AppConfig extends Application {
-
 }
